479aa997fecd8fd198fbfc60191f8d136212dd14
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-23 15:35:36 -0700

    Added requirements.txt Revising the templates to show content properly

--- journalapp/tests/test_auth.py
+++ journalapp/tests/test_auth.py
@@ -2,27 +2,17 @@
 import os
 
 
-def test_un_exist(app):
+def test_un_exist(auth_env):
     assert os.environ.get('AUTH_USERNAME', None) is not None
 
 
-def test_pw_exist(app):
+def test_pw_exist(auth_env):
     assert os.environ.get('AUTH_PASSWORD', None) is not None
 
 
 def test_password_is_encrypted(auth_env):
     """Test the password is encrypted."""
     assert os.environ.get('AUTH_PASSWORD', None) != 'SecretPwd!'
-
-
-def test_secure_view(app):
-    response = app.get('/secure', status=403)
-    assert response.status_code == 403
-
-
-def test_accesss_view(app):
-    response = app.get('/secure', status=200)
-    assert response.status_code == 200
 
 
 def test_check_pw_success(auth_env):
@@ -35,6 +25,16 @@ def test_check_pw_fail(auth_env):
     from journalapp.security import is_admin_pw
     password = '!dwPterceS'
     assert not is_admin_pw(password)
+
+
+def test_secure_view(app):
+    response = app.get('/secure', status=403)
+    assert response.status_code == 403
+
+
+def test_accesss_view(app):
+    response = app.get('/secure', status=200)
+    assert response.status_code == 200
 
 
 def test_get_login_view(app):
