213c78f18a6f664d7cee9fea2db47dd7e4c81ac7
Jeremy Edwards <jeremytedwards@gmail.com>
2016-03-24 17:50:24 -0700

    Revision to jinja2 date filters

--- journalapp/jinja2_filters.py
+++ journalapp/jinja2_filters.py
@@ -1,16 +1,5 @@
 # coding=utf-8
 
-# def datetimeformat(value, format='%H:%M / %d-%m-%Y'):
-#     return value.strftime(format)
-
-
-def dateformat(date):
-    return date.strftime('%Y-%m-%d')
-
-
-def datetimeformat(date):
-    return date.strftime('%Y-%m-%d %I:%M %p')
-
 
 def dateformat_monthfull(date):
     return date.strftime('%B')
